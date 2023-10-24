16e4a268ff92c2aa77926b86dde7228ab8bbcac8
smfields896 <smfields896@gmail.com>
2023-10-23 21:07:53 -0400

    Move registrations to service modules

--- src/CounterService/DotnetCounterService/API/Program.cs
+++ src/CounterService/DotnetCounterService/API/Program.cs
@@ -1,51 +1,17 @@
 using DistributedCounter.CounterService.API.GRPC.Services;
-using DistributedCounter.CounterService.Application.Common;
-using DistributedCounter.CounterService.Application.Common.Locking;
-using DistributedCounter.CounterService.Application.Counters.CreateCounter;
-using DistributedCounter.CounterService.Domain.Locking;
 using DistributedCounter.CounterService.Domain.Persistence;
-using Microsoft.EntityFrameworkCore;
-using RedLockNet;
-using RedLockNet.SERedis.Configuration;
-using StackExchange.Redis;
+using DistributedCounter.CounterService.Utilities.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-// Add services to the container.
-builder.Services.AddGrpc(opts =>
+builder.Services.RegisterFromServiceModules(servicesAvailableToModules: services =>
 {
-    opts.EnableDetailedErrors = true;
+    services.AddSingleton<IConfiguration>(builder.Configuration);
+    services.AddSingleton(builder.Environment);
 });
-builder.Services.AddGrpcReflection();
-builder.Services.AddMediatR(opts =>
-{
-    opts.RegisterServicesFromAssemblyContaining<CreateCounterCommand>();
-});
-builder.Services.AddDbContext<ApplicationDbContext>(opts => 
-    opts.UseNpgsql(builder.Configuration.GetConnectionString("Database"))
-        .EnableDetailedErrors()
-        .EnableSensitiveDataLogging()
-);
-builder.Services.AddScoped<IApplicationDbContext>(sp => sp.GetRequiredService<ApplicationDbContext>());
-builder.Services.AddSingleton<IConnectionMultiplexer>(
-    ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("Redis"))
-);
-builder.Services.AddSingleton<IDistributedLockFactory>(sp =>
-{
-    var redisConnection = sp.GetRequiredService<IConnectionMultiplexer>();
-    var redLockConnections = new List<RedLockMultiplexer>
-    {
-        new(redisConnection)
-    };
-
-    return RedLockNet.SERedis.RedLockFactory.Create(redLockConnections);
-});
-builder.Services.AddScoped<ILockFactory, RedLockFactory>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.MapGrpcService<CounterService>();
 app.MapGrpcReflectionService();
 
@@ -53,6 +19,6 @@ using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     await dbContext.Database.EnsureCreatedAsync();
-
 }
+
 app.Run();
--- src/CounterService/DotnetCounterService/Utilities/DependencyInjection/ServiceModuleExtensions.cs
+++ src/CounterService/DotnetCounterService/Utilities/DependencyInjection/ServiceModuleExtensions.cs
@@ -1,31 +1,25 @@
-﻿using System.Reflection;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Hosting;
 
 namespace DistributedCounter.CounterService.Utilities.DependencyInjection;
 
 public static class ServiceModuleExtensions
 {
-    public static void RegisterAssemblyModules(
-        this IHostApplicationBuilder host, 
-        Assembly assembly,
-        Action<IServiceCollection>? serviceModuleServices = null
+    public static void RegisterFromServiceModules(
+        this IServiceCollection services,
+        Action<IServiceCollection>? servicesAvailableToModules = null
     )
     {
         // Hold services including ServiceModules and dependencies they may require
         var serviceCollection = new ServiceCollection();
         
         // Add service module dependencies
-        serviceCollection.AddSingleton<IConfiguration>(host.Configuration);
-        serviceCollection.AddSingleton(host.Environment);
-        serviceModuleServices?.Invoke(serviceCollection);
+        servicesAvailableToModules?.Invoke(serviceCollection);
 
         // Scan for all service modules and register with DI container
-        assembly
-            .GetTypes()
-            .Where(type => type.IsAssignableTo(typeof(ServiceModule)))
+        AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes())
+            .Where(type => type.IsAssignableTo(typeof(ServiceModule)) && !type.IsAbstract)
             .ToList()
             .ForEach(type => serviceCollection.AddSingleton(typeof(ServiceModule), type));
         
@@ -36,10 +30,10 @@ public static class ServiceModuleExtensions
         // Load services from each module
         foreach (var module in serviceModules)
         {
-            module.Load(host.Services);
+            module.Load(services);
         }
         
         // Cleanup all service modules added
-        host.Services.RemoveAll(typeof(ServiceModule));
+        services.RemoveAll(typeof(ServiceModule));
     }
 }
