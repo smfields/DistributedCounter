ab83210dd6010726af7ee0efabe1b93ec35aa9bf
smfields896 <smfields896@gmail.com>
2023-10-25 13:34:54 -0400

    Add grain log context

--- src/CounterService/DotnetCounterService/Application/Common/GrainFilters/GrainFiltersServiceModule.cs
+++ src/CounterService/DotnetCounterService/Application/Common/GrainFilters/GrainFiltersServiceModule.cs
@@ -9,5 +9,6 @@ public class GrainFiltersServiceModule : ServiceModule
     {
         services.AddSingleton<IIncomingGrainCallFilter, IncomingRequestContextFilter>();
         services.AddSingleton<IOutgoingGrainCallFilter, OutgoingRequestContextFilter>();
+        services.AddSingleton<IIncomingGrainCallFilter, LoggingFilter>();
     }
 }
---
+++ src/CounterService/DotnetCounterService/Application/Common/GrainFilters/LoggingFilter.cs
@@ -0,0 +1,17 @@
+﻿using Serilog.Context;
+
+namespace DistributedCounter.CounterService.Application.Common.GrainFilters;
+
+public class LoggingFilter : IIncomingGrainCallFilter
+{
+    public async Task Invoke(IIncomingGrainCallContext context)
+    {
+        using (LogContext.PushProperty("SourceGrainId", context.SourceId))
+        using (LogContext.PushProperty("GrainId", context.TargetId))
+        {
+            await context.Invoke();
+        }
+    }
+}
+
+
