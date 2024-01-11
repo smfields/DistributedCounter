faee48633e55d18cf7feeb5afe7e59bcdc6ce979
Sam Fields <smfields896@gmail.com>
2024-01-10 22:25:42 -0500

    Fix compose files

--- src/CounterService/API/Common/Telemetry/TelemetryServiceModule.cs
+++ src/CounterService/API/Common/Telemetry/TelemetryServiceModule.cs
@@ -8,34 +8,34 @@ public class TelemetryServiceModule(IConfiguration configuration) : ServiceModul
 {
     public override void Load(IServiceCollection services)
     {
-        // var zipkinOptions = configuration.GetOptions<ZipkinOptions>();
-        //
-        // AppContext.SetSwitch("Azure.Experimental.EnableActivitySource", true);
-        //
-        // services
-        //     .AddOpenTelemetry()
-        //     .WithTracing(tracing =>
-        //     {
-        //         tracing.SetResourceBuilder(
-        //             ResourceBuilder
-        //                 .CreateDefault()
-        //                 .AddService(serviceName: "CounterService-Dotnet", serviceVersion: "1.0.0")
-        //         );
-        //
-        //         tracing.AddAspNetCoreInstrumentation();
-        //         tracing.AddSource("Microsoft.Orleans.Runtime");
-        //         tracing.AddSource("Microsoft.Orleans.Application");
-        //         tracing.AddSource("Azure.Cosmos.Operation");
-        //         
-        //         if (zipkinOptions.Enabled)
-        //         {
-        //             tracing.AddZipkinExporter(zipkin =>
-        //             {
-        //                 zipkin.Endpoint = new Uri(zipkinOptions.Endpoint);
-        //                 zipkin.BatchExportProcessorOptions.MaxQueueSize = 4096 * 10;
-        //                 zipkin.BatchExportProcessorOptions.ScheduledDelayMilliseconds = 2500;
-        //             });
-        //         }
-        //     });
+        var zipkinOptions = configuration.GetOptions<ZipkinOptions>();
+        
+        AppContext.SetSwitch("Azure.Experimental.EnableActivitySource", true);
+        
+        services
+            .AddOpenTelemetry()
+            .WithTracing(tracing =>
+            {
+                tracing.SetResourceBuilder(
+                    ResourceBuilder
+                        .CreateDefault()
+                        .AddService(serviceName: "CounterService-Dotnet", serviceVersion: "1.0.0")
+                );
+        
+                tracing.AddAspNetCoreInstrumentation();
+                tracing.AddSource("Microsoft.Orleans.Runtime");
+                tracing.AddSource("Microsoft.Orleans.Application");
+                tracing.AddSource("Azure.Cosmos.Operation");
+                
+                if (zipkinOptions.Enabled)
+                {
+                    tracing.AddZipkinExporter(zipkin =>
+                    {
+                        zipkin.Endpoint = new Uri(zipkinOptions.Endpoint);
+                        zipkin.BatchExportProcessorOptions.MaxQueueSize = 4096 * 10;
+                        zipkin.BatchExportProcessorOptions.ScheduledDelayMilliseconds = 2500;
+                    });
+                }
+            });
     }
 }
